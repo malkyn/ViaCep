bf6238b69781e895933fdbb26040b8c240c9320e
vinir <46539805+malkyn@users.noreply.github.com>
2022-09-19 18:08:31 -0300

    Delete ViaCepTeste/Configuration directory

--- ViaCepTeste/Configuration/CepConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿//using ViaCepTeste.Interfaces;
-
-//namespace ViaCepTeste.Configuration
-//{
-//    public class CepConfig : ICepConfig
-//    {
-//        public string BaseUrl { get; set; }
-//    }
-//}
